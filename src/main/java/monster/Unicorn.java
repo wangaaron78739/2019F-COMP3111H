26c205c680a03114e01bcd9e832c1e320dadf0de
Alex CHIU <khchiuac@connect.ust.hk>
2019-11-04 18:51:35 +0800

    Catapult canAttack ready

--- src/main/java/monster/Unicorn.java
+++ src/main/java/monster/Unicorn.java
@@ -2,10 +2,23 @@ package monster;
 
 import arena.logic.Arena;
 
+/**
+ * 
+ * Class implement one type of Monster, Unicorn
+ * This type of monster has the largest HP
+ * @author CHIU Ka Ho
+ * 
+ */
 public class Unicorn extends Monster {
     private static final int defaultHP = 150;
     private static final int defaultSpeed = 5;
 
+    /**
+     * Unicorn Constructor
+     * @param x The x-coordinate (in pixels) of the Unicorn
+     * @param y The y-coordinate (in pixels) of the Unicorn
+     * @param stage The stage of the game now
+     */
     public Unicorn(int x, int y, int stage) {
         super(x, y, defaultSpeed, defaultHP*stage, "Unicorn"); // stronger as more HP
     }
