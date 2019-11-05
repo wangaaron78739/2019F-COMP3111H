0e0d6d8d905c5a914e4b6af4ae94ba4b8a6fa94e
Alex CHIU <khchiuac@connect.ust.hk>
2019-11-05 23:09:12 +0800

    Small fix and part of javadoc for Monster

--- src/main/java/monster/Unicorn.java
+++ src/main/java/monster/Unicorn.java
@@ -4,8 +4,9 @@ import arena.logic.Arena;
 
 /**
  * 
- * Class implement one type of Monster, Unicorn
- * This type of monster has the largest HP
+ * Class implement one type of Monster, Unicorn.
+ * Unicorn has more HP than other monsters.
+ * Along the time elapsed, Unicorn would become stronger as they would have higher HP.
  * @author CHIU Ka Ho
  * 
  */
@@ -14,10 +15,10 @@ public class Unicorn extends Monster {
     private static final int defaultSpeed = 2;
 
     /**
-     * Unicorn Constructor
+     * Unicorn Constructor.
      * @param x The x-coordinate (in pixels) of the Unicorn
      * @param y The y-coordinate (in pixels) of the Unicorn
-     * @param stage The stage of the game now
+     * @param stage The current stage of the game
      */
     public Unicorn(int x, int y, int stage) {
         super(x, y, defaultSpeed, defaultHP*stage, "Unicorn"); // stronger as more HP
