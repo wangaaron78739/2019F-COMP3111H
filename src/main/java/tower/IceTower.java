09ec05617413e401752d9c344d2936db919e05fb
jrenaf <jrenaf@connect.ust.hk>
2019-11-12 18:54:13 +0800

    [Tower] revise IceTower, make some changes in LaserTower

--- src/main/java/tower/IceTower.java
+++ src/main/java/tower/IceTower.java
@@ -2,18 +2,60 @@ package tower;
 
 import static arena.logic.ArenaConstants.*;
 import arena.logic.Resource;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import monster.Monster;
 
+/**
+ * Class for IceTower, which can slow down the movement of the monster for a period of time
+ * @author REN Jiming
+ *
+ */
 
 public class IceTower extends Tower {
-    private static final int baseAttackPower = 100;
+    private static final int baseSlowDownTime = 100;
+    private static final int upgradeSlowDownTime = 50;
     private static final int baseBuildingCost = 100;
     private static final int baseShootingRange = 100;
-    private static final int baseAttackCooldown = 100;
     private static final String typeName = "Ice";
 
+    /**
+     * IceTower Constructor
+     * @param x The x-coordinate of the cell where the BasicTower locates
+     * @param y The y-coordinate of the cell where the BasicTower locates
+     */
     public IceTower(int x, int y) {
-        super(baseAttackPower, baseBuildingCost, baseShootingRange, x, y, typeName);
+        super(baseSlowDownTime, baseBuildingCost, baseShootingRange, x, y, typeName);
+    }
+    
+    /**
+     * Overriding method to slow down the movement of the target monster
+     * @param target The monster whose speed to be decreased
+     */
+    @Override
+    public void implement(Monster target){
+    	target.setCoolDown(getAttackPower());
+    }
+    
+    /**
+     * Overriding method to upgrade the IceTower
+     * <p>
+     * Increase the time to freeze the target monster and consume certain amount of resource.
+     * <p>
+     * Check the remained resource beforehand; if there is no enough resource to exploit, give alert
+     */
+    @Override
+    public void upgrade(){
+    	if(Resource.canDeductAmount(getUpgradeCost())){
+    	 setAttackPower(getAttackPower() + upgradeSlowDownTime);
+    	 Resource.deductAmount(getUpgradeCost());
+    	}
+    	else{
+    		Alert alert = new Alert(Alert.AlertType.INFORMATION, "", ButtonType.CLOSE);
+            alert.setTitle("Cannot upgrade");
+            alert.setHeaderText("No enough resource, you need " + getUpgradeCost() + " resource to upgrade");
+            alert.showAndWait();
+    	}
     }
     
     /**
@@ -25,17 +67,9 @@ public class IceTower extends Tower {
      */
     @Override
     public boolean canAttack(int xPx, int yPx) {
-    	return (Math.hypot(xPx - (getX()*GRID_WIDTH+GRID_WIDTH/2), yPx - (getY()*GRID_HEIGHT+GRID_HEIGHT/2))<=getShootingRange());
-    }
-    
-    @Override
-    public void upgrade(){
-        setAttackPower(this.getAttackPower() + 60);
-    	 Resource.deductAmount(this.getUpgradeCost()); 
-    }
-    
-    @Override
-    public void implement(Monster target){
-    	target.setCoolDown(baseAttackPower);
+    	return (Math.hypot(
+    			xPx - (getX() * GRID_WIDTH + GRID_WIDTH/2), 
+    			yPx - (getY() * GRID_HEIGHT + GRID_HEIGHT/2))
+    			<= getShootingRange());
     }
 }
--- src/main/java/tower/LaserTower.java
+++ src/main/java/tower/LaserTower.java
@@ -51,11 +51,6 @@ public class LaserTower extends Tower {
 				double distanceToEndZone = Math.hypot(	
     					m.getXPx() - (MAX_V_NUM_GRID + 0.5) * GRID_WIDTH,
     					m.getYPx() - (MAX_H_NUM_GRID + 0.5) * GRID_HEIGHT);
-//				if(!map.isEmpty() && distanceToEndZone < map.entrySet().iterator().next().getValue()){
-//					map.remove(map.entrySet().iterator().next());
-//					map.put(m, distanceToEndZone);
-//				}
-//				else map.put(m, distanceToEndZone);
 				if(!map.isEmpty()){
 					HashMap.Entry<Monster, Double> set =  map.entrySet().iterator().next();
 					if(distanceToEndZone < set.getValue()){
@@ -66,35 +61,43 @@ public class LaserTower extends Tower {
 				else map.put(m,distanceToEndZone);
 			}
 			if(map.isEmpty()) return;
-			double targetMonX = (map.entrySet().iterator().next().getKey().getXGrid() + 0.5) * ArenaConstants.GRID_WIDTH;
-    		double targetMonY = (map.entrySet().iterator().next().getKey().getYGrid() + 0.5) * ArenaConstants.GRID_HEIGHT;
-			int towerX = this.getX() * ArenaConstants.GRID_WIDTH + ArenaConstants.GRID_WIDTH/2;
-			int towerY = this.getY() * ArenaConstants.GRID_HEIGHT + ArenaConstants.GRID_HEIGHT/2;
-    		double slopeReciprocal = (targetMonX - towerX)/(targetMonY - towerY); 
-    		double frameX = 0, frameY = 0;
-    		if(targetMonY > towerY){
-    			frameY = (ArenaConstants.MAX_H_NUM_GRID + 1) * ArenaConstants.GRID_HEIGHT;
-    			frameX = ((ArenaConstants.MAX_H_NUM_GRID + 1) * ArenaConstants.GRID_HEIGHT - towerY) * slopeReciprocal + towerX;
-			}
-    		if(targetMonY <= towerY){
-    			frameX = (0 - towerY) * slopeReciprocal + towerX;
-    		}
-			attackTrace = new Line(towerX, towerY, frameX, frameY);
-			double slope = 1 / slopeReciprocal;
-			double deltaY = 3 * Math.hypot(1 , slope);
+			
+			double targetMonX = (map.entrySet().iterator().next().getKey().getXGrid() + 0.5) * GRID_WIDTH;
+    		double targetMonY = (map.entrySet().iterator().next().getKey().getYGrid() + 0.5) * GRID_HEIGHT;
+			int towerX = getX() * GRID_WIDTH + GRID_WIDTH/2;
+			int towerY = getY() * GRID_HEIGHT + GRID_HEIGHT/2;
 			ArrayList<Monster> targetMonList = new ArrayList<Monster>();
-			for(Monster m : Arena.getMonsters()){
-				if(m.getYPx() > slope * (m.getXPx() - towerX) - deltaY &&
-				   m.getYPx() < slope * (m.getXPx() - towerX) + deltaY){
-					System.out.printf("$$$In attack range$$$\n");
-					targetMonList.add(m);
-					//if(targetMonY > towerY && m.getYPx() > towerY) targetMonList.add(m);
-					//if(targetMonY < towerY && m.getYPx() < towerY) targetMonList.add(m);
+			
+			//if the attacking trace is not a horizontal line:
+			if(targetMonY != towerY){
+				double slopeReciprocal = (targetMonX - towerX)/(targetMonY - towerY); 
+				double frameX = 0, frameY = 0;
+				if(targetMonY > towerY){
+					frameY = (MAX_H_NUM_GRID + 1) * GRID_HEIGHT;
+					frameX = ((MAX_H_NUM_GRID + 1) * GRID_HEIGHT - towerY) * slopeReciprocal + towerX;
+				}
+				if(targetMonY <= towerY){
+					frameX = (0 - towerY) * slopeReciprocal + towerX;
+				}
+				attackTrace = new Line(towerX, towerY, frameX, frameY);
+				
+				double slope = 1 / slopeReciprocal;
+				double deltaY = 3 * Math.hypot(1 , slope);
+				for(Monster m : Arena.getMonsters()){
+					if(m.getYPx() > slope * (m.getXPx() - towerX) - deltaY &&
+					   m.getYPx() < slope * (m.getXPx() - towerX) + deltaY){
+						System.out.printf("$$$In attack range$$$\n");
+						targetMonList.add(m);
+						//if(targetMonY > towerY && m.getYPx() > towerY) targetMonList.add(m);
+						//if(targetMonY < towerY && m.getYPx() < towerY) targetMonList.add(m);
+					}
 				}
 			}
 			for(Monster tm : targetMonList){
 				Arena.logAttack(this, tm);
     			this.implement(tm);
+    			tm.setHit(true);
+        		Arena.setTowerShot(getX(),getY());
     			Resource.deductAmount(10);
 			}
 		}
