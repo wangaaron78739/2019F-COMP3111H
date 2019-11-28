725b8d22922b92b5763021709517287cf6fe289e
REN Jiming <jrenaf@connect.ust.hk>
2019-11-28 11:50:00 +0800

    Add files via upload

---
+++ SpringServer/src/main/java/SpringServer/Greeting.java
@@ -0,0 +1,29 @@
+package SpringServer;
+
+import java.util.LinkedList;
+
+public class Greeting {
+
+  private final long id;
+  private final String content;
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+	
+//	private LinkedList<Integer> scoreList;
+//	public Greeting(){
+//		scoreList = new LinkedList<Integer>();
+//	}
+//	
+	
+}
